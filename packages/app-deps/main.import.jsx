d6703d10d3cb6cb00bcac7fa78f0d7a383a0953f
DeAndre Johnson <dre@phreshr.com>
2015-07-14 23:30:47 -0400

    es6ified collections, added app-dep package, added user auth logic

---
+++ packages/app-deps/main.import.jsx
@@ -0,0 +1,9 @@
+/* jshint esnext: true */
+/* global Dependencies */
+"use strict";
+
+export default Dependencies;
+
+export const _ = Dependencies._;
+export const moment = Dependencies.moment;
+export const classNames = Dependencies.classNames;
