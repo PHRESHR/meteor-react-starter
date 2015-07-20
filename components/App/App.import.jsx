42d771e39f0a9e17b20c9a84503546a457f6eb75
DeAndre Johnson <dre@phreshr.com>
2015-07-20 17:00:47 -0400

    removed bootstrap css and restructured app layout

--- components/App/App.import.jsx
+++ components/App/App.import.jsx
@@ -3,24 +3,29 @@ export default class App extends React.Component {
     super(...args);
   }
   render() {
-    return (
-      <div>
-        <nav brand={<a href="/">Tomorrow Pictures</a>} staticTop={true}>
-          <a href="/">Tomorrow Pictures</a>
-          <ul className="nav navbar-nav">
-            <li><a href="/">Home</a></li>
-            <li><a href="#">Another Link</a></li>
-          </ul>
-          <ul className="nav navbar-nav navbar-right">
-            <li><a href="http://tomorrowpictures.com" target="_blank">Visit Tomorrow Pictures Inc.</a></li>
-          </ul>
-        </nav>
-        <section>
-          <div>
-            { this.props.content }
-          </div>
-        </section>
+    let navBar = (
+      <nav>
+        <a href="/">Tomorrow Pictures</a>
+        <ul className="nav navbar-nav">
+          <li><a href="/">Home</a></li>
+          <li><a href="#">Another Link</a></li>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+          <li><a href="http://tomorrowpictures.com" target="_blank">Visit Tomorrow Pictures Inc.</a></li>
+        </ul>
+      </nav>
+    );
+    let mainContent = (
+      <main>
+        { this.props.content }
+      </main>
+    );
+    let appCanvas = (
+      <div className="wrap">
+        { navBar }
+        { mainContent }
       </div>
     );
+    return appCanvas;
   }
 }
