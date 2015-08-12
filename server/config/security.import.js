5e5866a30f3b8e2a7b7845bccaa907e6cefedcc9
DeAndre Johnson <dre@phreshr.com>
2015-08-12 00:15:57 -0400

    update meteor deps

--- server/config/security.import.js
+++ server/config/security.import.js
@@ -1,11 +1,8 @@
 BrowserPolicy.content.allowEval();
+
+// for google
 BrowserPolicy.content.allowOriginForAll('*.googleapis.com');
 BrowserPolicy.content.allowOriginForAll('*.gstatic.com');
-BrowserPolicy.content.allowOriginForAll('*.bootstrapcdn.com');
-BrowserPolicy.content.allowOriginForAll('*.semantic-ui.com');
-
-BrowserPolicy.content.allowOriginForAll('*.videogular.com');
-BrowserPolicy.content.allowOriginForAll('*.placehold.it');
 
 BrowserPolicy.content.allowOriginForAll('*.google-analytics.com');
 BrowserPolicy.content.allowOriginForAll('*.stats.g.doubleclick.net');
